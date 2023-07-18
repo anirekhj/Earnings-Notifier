9ff9822bc772dba8ebce142ca149aaec22f64d5c
Anirekh Jain <16602409+anirekhj@users.noreply.github.com>
2023-07-17 23:38:14 -0400

    Handle sorting edge case
    There is an edge case where multiple companies can have same ticker name although they are listed on different exchanges. The descending order sort should only take the tickers in account IF their earnings are about to be released within 30 days.

--- scraper.py
+++ scraper.py
@@ -25,8 +25,8 @@ exchanges_file = "exchanges.txt"
 with open(exchanges_file, 'r') as file:
     exchanges = [line.strip() for line in file]
 
-# Initialize a dictionary to store ticker details for each exchange
-ticker_details = {}
+# Initialize a list to store ticker details for earnings within 30 days
+ticker_details_within_30_days = []
 
 # Define a function to fetch earnings data for a given ticker and exchange
 async def fetch_earnings(session, ticker, exchange):
@@ -49,7 +49,7 @@ async def fetch_earnings(session, ticker, exchange):
             days_until_earnings = (earnings_date - current_date).days
 
             if 0 <= days_until_earnings <= 30:
-                return (ticker, exchange, days_until_earnings, earnings_date, ' '.join(values[1:]))
+                ticker_details_within_30_days.append((ticker, exchange, days_until_earnings, earnings_date, ' '.join(values[1:])))
 
 # Start measuring the execution time
 start_time = time.time()
@@ -61,25 +61,18 @@ async def main():
             for exchange in exchanges:
                 tasks.append(fetch_earnings(client, ticker, exchange))
 
-        ticker_results = await asyncio.gather(*tasks)
+        await asyncio.gather(*tasks)
 
-    # Store ticker details for each exchange in the dictionary
-    for result in ticker_results:
-        if result is not None:
-            ticker, exchange, days_until_earnings, earnings_date, additional_values = result
-            if ticker in ticker_details:
-                ticker_details[ticker].append((exchange, days_until_earnings, earnings_date, additional_values))
-            else:
-                ticker_details[ticker] = [(exchange, days_until_earnings, earnings_date, additional_values)]
+    # Sort ticker details in descending order based on days until earnings
+    ticker_details_within_30_days.sort(key=lambda x: x[2], reverse=True)
 
-    # Print the ticker details
-    for ticker, details in ticker_details.items():
+    # Print the sorted ticker details
+    for ticker, exchange, days_until_earnings, earnings_date, additional_values in ticker_details_within_30_days:
         print(f"Ticker: {ticker}")
-        for exchange, days_until_earnings, earnings_date, additional_values in details:
-            print(f"  Exchange: {exchange}")
-            print(f"  Earnings date is {days_until_earnings} days from today: {earnings_date.strftime('%b. %d, %Y')}")
-            print(f"  {additional_values}")
-            print()
+        print(f"  Exchange: {exchange}")
+        print(f"  Earnings date is {days_until_earnings} days from today: {earnings_date.strftime('%b. %d, %Y')}")
+        print(f"  {additional_values}")
+        print()
 
     # Calculate the elapsed time
     elapsed_time = time.time() - start_time
