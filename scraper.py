e1c00aaf2f2b64e7bc2cc25594fb1c8ef3811436
Anirekh Jain <16602409+anirekhj@users.noreply.github.com>
2023-07-17 23:17:17 -0400

    Moar optimize

--- scraper.py
+++ scraper.py
@@ -1,9 +1,10 @@
 import argparse
-import concurrent.futures
+import asyncio
 import requests
 from bs4 import BeautifulSoup as BS
 from datetime import datetime
 import time
+import httpx
 
 # Create argument parser
 parser = argparse.ArgumentParser(description='Scrape earnings data for a list of stock symbols.')
@@ -13,7 +14,6 @@ parser.add_argument('input_file', type=str, help='Input file containing a list o
 args = parser.parse_args()
 
 # Read the tickers from the input file
-tickers = []
 with open(args.input_file, 'r') as file:
     tickers = file.read().splitlines()
 
@@ -22,24 +22,23 @@ base_url = 'https://www.marketbeat.com'
 
 # Read the exchanges from the input file
 exchanges_file = "exchanges.txt"
-exchanges = []
 with open(exchanges_file, 'r') as file:
-    for line in file:
-        exchange = line.strip()
-        exchanges.append(exchange)
+    exchanges = [line.strip() for line in file]
 
 # Initialize a list to store ticker details
 ticker_details = []
 
 # Define a function to fetch earnings data for a given ticker and exchange
-def fetch_earnings(ticker, exchange):
+async def fetch_earnings(session, ticker, exchange):
     url = f'{base_url}/stocks/{exchange}/{ticker}/earnings/'
     agent = {
         "User-Agent": 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'
     }
-    response = requests.get(url, headers=agent)
-    if response.ok:
-        soup = BS(response.content, 'html.parser')
+    response = await session.get(url, headers=agent)
+    if response.status_code == 200:
+        content = response.content
+        soup = BS(content, 'lxml')  # Use lxml parser for faster parsing
+
         element = soup.find('dd', class_='stat-summary-heading my-1')
         if element:
             values = list(element.stripped_strings)
@@ -55,35 +54,33 @@ def fetch_earnings(ticker, exchange):
 # Start measuring the execution time
 start_time = time.time()
 
-# Process each ticker and exchange using multiprocessing
-with concurrent.futures.ThreadPoolExecutor() as executor:
-    # Use concurrent futures to execute fetch_earnings for each ticker and exchange combination
-    futures = []
-    for stock_symbol in tickers:
-        for exchange in exchanges:
-            futures.append(executor.submit(fetch_earnings, stock_symbol, exchange))
+async def main():
+    tasks = []
+    async with httpx.AsyncClient() as client:
+        for ticker in tickers:
+            for exchange in exchanges:
+                tasks.append(fetch_earnings(client, ticker, exchange))
 
-    # Collect the results from completed futures
-    ticker_details = []
-    for future in concurrent.futures.as_completed(futures):
-        result = future.result()
-        if result is not None:
-            ticker_details.append(result)
+        ticker_details = await asyncio.gather(*tasks)
 
-# Sort the ticker details based on days until earnings in descending order
-ticker_details.sort(key=lambda x: x[2], reverse=True)
+    # Remove None values and sort the ticker details based on days until earnings in descending order
+    ticker_details = [td for td in ticker_details if td is not None]
+    ticker_details.sort(key=lambda x: x[2], reverse=True)
 
-# Print the ticker details
-for ticker_detail in ticker_details:
-    stock_symbol, exchange, days_until_earnings, earnings_date, additional_values = ticker_detail
-    print(f"Ticker: {stock_symbol}")
-    print(f"Exchange: {exchange}")
-    print(f"Earnings date is {days_until_earnings} days from today: {earnings_date.strftime('%b. %d, %Y')}")
-    print(additional_values)
-    print()
+    # Print the ticker details
+    for ticker_detail in ticker_details:
+        stock_symbol, exchange, days_until_earnings, earnings_date, additional_values = ticker_detail
+        print(f"Ticker: {stock_symbol}")
+        print(f"Exchange: {exchange}")
+        print(f"Earnings date is {days_until_earnings} days from today: {earnings_date.strftime('%b. %d, %Y')}")
+        print(additional_values)
+        print()
 
-# Calculate the elapsed time
-elapsed_time = time.time() - start_time
+    # Calculate the elapsed time
+    elapsed_time = time.time() - start_time
 
-# Print the elapsed time with 3 decimal places
-print(f"Execution time: {round(elapsed_time, 3)} seconds")
+    # Print the elapsed time with 3 decimal places
+    print(f"Execution time: {round(elapsed_time, 3)} seconds")
+
+# Run the event loop
+asyncio.run(main())
